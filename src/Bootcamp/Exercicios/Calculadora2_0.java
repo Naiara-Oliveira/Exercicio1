6ef057536d3e3fb6e210771880790503b5bd1d1c
Naiara Oliveira <naiaraoliveira.9722@gmail.com>
2022-05-02 18:08:35 -0400

    primeiro exercicio

---
+++ src/Bootcamp/Exercicios/Calculadora2_0.java
@@ -0,0 +1,37 @@
+package Bootcamp.Exercicios;
+
+public class Calculadora2_0 {
+    public static void main(String[] args) {
+
+        somarValor( 9,4 );
+        subitrairValor( 10,2 );
+        multiplicarValor( 10,2 );
+        dividirValor( 10, 2 );
+    }
+
+    public static void  somarValor(double a, double b) {
+        double soma = a + b;
+        System.out.println("A soma de " + a +" mais " + b +" = " + soma );
+
+    }
+
+    public static void subitrairValor(double a, double b) {
+        double subtrair = a - b;
+        System.out.println("A subtração de " + a +" menos " + b +" = " + subtrair );
+
+    }
+
+    public static void multiplicarValor(double a, double b) {
+        double multiplicar = a * b;
+        System.out.println("A multiplicação de "  + a +" vezes " + b +" = "  + multiplicar );
+
+    }
+
+    public static void dividirValor(double a, double b) {
+        double dividir = a / b;
+        System.out.println("A divisão de " + a +" dividido " + b +" = "  + dividir);
+
+    }
+
+}
+
---
+++ src/Bootcamp/Exercicios/Emprestimos.java
@@ -0,0 +1,46 @@
+package Bootcamp.Exercicios;
+
+public class Emprestimos {
+    public static void main(String[] args) {
+
+        System.out.println(calcularTaxaParcelas( 120, 3 ));
+
+
+    };
+
+    public static int getDuasParcelas() {
+        return 2;
+    }
+
+    public static int getTresParcelas() {
+        return 3;
+    }
+
+    public static double getTaxaDuasParcelas() {
+        return 0.3;
+    }
+    public static double getTaxaTresParcelas() {
+        return 0.45;
+    }
+    public  static double calcularTaxaParcelas(double valor, int parcelas){
+
+        if (parcelas == 2){
+
+            double valorFinal = valor + (valor * getTaxaDuasParcelas());
+            double taxaPagar = valorFinal - valor;
+            System.out.println("Valor do emprestimo " + valor);
+            System.out.println("Valor final para as duas parcelas: R$ " + valorFinal);
+            System.out.println("O valor da taxa é de: " + taxaPagar);
+        }
+        else if (parcelas == 3) {
+            double valorFinal = valor + (valor * getTaxaTresParcelas());
+            double taxaPagar = valorFinal - valor;
+            System.out.println("Valor do emprestimo " + valor);
+            System.out.println("Valor final para as tres parcelas: R$ " + valorFinal);
+            System.out.println("O valor da taxa é de: " + taxaPagar);
+        }else{
+            System.out.println("Quantidades de parcelas não aceitas");
+        }
+        return valor;
+    }
+}
